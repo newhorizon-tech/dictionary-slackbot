4fce08ad6489095950bb7e4f2c2c4e0c30eb0c96
newhorizon-tech <alaukikinbox@gmail.com>
2020-11-09 19:06:37 +0530

    Create test bot

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'activesupport'
+gem 'async-websocket', '~>0.8.0'
 gem 'dotenv'
+gem 'puma'
 gem 'slack-ruby-bot'
--- main.rb
+++ main.rb
@@ -1,6 +1,26 @@
+#!/usr/bin/ruby
+
 require 'slack-ruby-bot'
+require 'dotenv'
 require_relative 'lib/bot.rb'
 require_relative 'lib/commands/test.rb'
 
 puts 'Hello bot'
 test_function
+
+Dotenv.load
+
+class TestBot < SlackRubyBot::Bot
+  command 'test' do |client, data, match|
+    client.say(text: 'Slack is working', channel: data.channel)
+    client.say(text: "Client is '#{client}''", channel: data.channel)
+    client.say(text: "Data is '#{data}''", channel: data.channel)
+    client.say(text: "Match is '#{match}''", channel: data.channel)
+  end
+end
+
+SlackRubyBot.configure do |config|
+  config.aliases = [':simp:', 'based']
+end
+
+TestBot.run
