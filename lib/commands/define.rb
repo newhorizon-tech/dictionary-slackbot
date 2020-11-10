bff05995cfed60db68062cc6cb41bca65a6e9997
newhorizon-tech <alaukikinbox@gmail.com>
2020-11-10 18:00:29 +0530

    Add isolate_word method

--- lib/commands/define.rb
+++ lib/commands/define.rb
@@ -1,10 +1,25 @@
+require 'open-uri'
+require 'json'
+
 module Dictionary
   module Commands
     class Define < SlackRubyBot::Commands::Base
       command 'define' do |client, data, match|
-        client.say(text: 'Slack is working', channel: data.channel)
-        client.say(text: "Match is '#{match}''", channel: data.channel)
+        client.say(text: 'The isolated word is', channel: data.channel)
+        # output = Tools.isolate_word(match)
+
+        client.say(text: "Match is #{match}", channel: data.channel)
+        # client.say(text: "Output is #{output}", channel: data.channel)
       end
     end
+  end
+end
+
+class Tools
+  def self.isolate_word(word)
+    items = word.split('define')
+    word = items[1].strip
+    puts word
+    word
   end
 end
