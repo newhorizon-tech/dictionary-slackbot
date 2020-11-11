b021437a3f146acbe311c69d5b0ff1228f55df17
newhorizon-tech <alaukikinbox@gmail.com>
2020-11-11 16:59:15 +0530

    RSpec fix 3

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -1,11 +1,8 @@
 require 'rspec/expectations'
-begin
-  require 'slack-ruby-bot/rspec'
-rescue LoadError
-  system 'bundle install'
-  # system 'rspec'
-  # exit
-end
+system 'bundle install'
+require 'slack-ruby-bot'
+require 'slack-ruby-bot/rspec'
+
 require_relative '../lib/bot.rb'
 require_relative '../lib/parser.rb'
 require_relative '../lib/commands/define.rb'
